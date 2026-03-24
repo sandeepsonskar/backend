954393cc25108b57fb9894f95e5c5830239d22d9
sandeep sonaskar <sandeepsonskar@gmail.com>
2026-03-24 15:01:05 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-const schema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-module.exports = mongoose.model("User", schema);
+module.exports = mongoose.model("User", UserSchema);
