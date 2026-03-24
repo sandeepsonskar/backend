fab6c7a84977267e7c9e34708481e7487a4ac6c9
sandeep sonaskar <sandeepsonskar@gmail.com>
2026-03-24 15:01:45 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,13 @@
 
+const jwt = require("jsonwebtoken");
+
+module.exports = function(req,res,next){
+  try{
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  }catch(err){
+    res.status(401).json({msg:"Unauthorized"});
+  }
+};
